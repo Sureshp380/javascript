fd0f66c1f295524f0cd8cdeb1435f0c2fbfd8aae
Suresh Pradhan <37619497+Sureshp380@users.noreply.github.com>
2024-12-08 22:03:03 +0530

    basic of Array Part 2

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -26,5 +26,5 @@ let score = 100;
 let score2 = 200;
 let score3 = 300;
 
-console.log(Array.of(score, score2, score3));
+console.log(Array.of(score, score2, score3)); // of : return a new set of arrays.
 
